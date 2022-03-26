6fdadc392e1abec9a4fd0c46ab3ce5cfe22845fd
Andrix <andrixtan_96@hotmail.com>
2022-03-26 12:58:18 +0800

    add click buttons display onscreen

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ const divide = (first, second) => {
 const operate = (first, second, operator) => {
 	return operator(first, second)
 }
+
+let display = ''
+let displayScreen = document.querySelector('.display');
+// displayScreen.addEventListener('change', (e) => {
+// 	displayScreen = display.textContent;
+// })
+
+const btns = document.querySelectorAll('.button')
+btns.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		// console.log(e);
+		display += e.target.textContent;
+		// console.log(display);
+		displayScreen.textContent = display;
+	})
+})
