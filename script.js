dc6615df93be23b3aec00698547b3e3ed0fad25a
Andrix <andrixtan_96@hotmail.com>
2022-03-26 12:25:36 +0800

    add html and css of calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = (first, second) => {
+	return first + second
+}
+
+const subtract = (first, second) => {
+	return first - second
+}
+
+const multiply = (first, second) => {
+	return first * second
+}
+
+const divide = (first, second) => {
+	return first / second
+}
+
+const operate = (first, second, operator) => {
+	return operator(first, second)
+}
