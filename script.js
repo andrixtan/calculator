2053e1a4caece15705d833dcca44741f0d040d37
Andrix <andrixtan_96@hotmail.com>
2022-04-06 15:54:00 +0800

    add function of backspace button

--- script.js
+++ script.js
@@ -5,45 +5,55 @@ const add = (first, second) => {
 	// else {
 	return parseInt(first) + parseInt(second);
 	// }
-}
+};
 const subtract = (first, second) => {
-	return first - second
-}
+	return first - second;
+};
 const multiply = (first, second) => {
-	return first * second
-}
+	return first * second;
+};
 const divide = (first, second) => {
-	return first / second
-}
+	return first / second;
+};
 const operate = (first, second, operator) => {
-	return operator(first, second)
-}
+	return operator(first, second);
+};
 
-let display = ''
-const displayScreen = document.querySelector('.display');
+let display = "";
+const displayScreen = document.querySelector(".display");
 
-const digitBtns = document.querySelectorAll('.digit')
+const digitBtns = document.querySelectorAll(".digit");
 digitBtns.forEach((btn) => {
-	btn.addEventListener('click', (e) => {
+	btn.addEventListener("click", (e) => {
 		display += e.target.textContent;
 		displayScreen.textContent = display;
 		// console.log(display);
-	})
-})
+	});
+});
 
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
+const backspaceBtn = document.querySelector(".backspace");
+backspaceBtn.addEventListener("click", (e) => {
+	if (
+		(!firstNumber && !operator) ||
+		(firstNumber && operator && !secondNumber)
+	) {
+		display = display.slice(0, display.length - 1);
+		displayScreen.textContent = display;
+	}
+});
 
-const operatorBtns = document.querySelectorAll('.operator')
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+const operatorBtns = document.querySelectorAll(".operator");
 operatorBtns.forEach((btn) => {
-	btn.addEventListener('click', (e) => {
+	btn.addEventListener("click", (e) => {
 		if (!firstNumber && !operator && !display) {
-			return
-		}
-		  else if (!operator) {
+			return;
+		} else if (!operator) {
 			firstNumber = displayScreen.textContent;
-			display = '';
+			display = "";
 			operator = e.target.textContent;
 		} else if (firstNumber && operator && !display) {
 			operator = e.target.textContent;
@@ -52,60 +62,60 @@ operatorBtns.forEach((btn) => {
 			secondNumber = display;
 			// console.log(operator)
 			// console.log(secondNumber)
-			display = '';
+			display = "";
 			displayScreen.textContent = evaluate(firstNumber, secondNumber, operator);
 			operator = e.target.textContent;
 			firstNumber = displayScreen.textContent;
-			if (firstNumber === 'Error!') {
-				firstNumber = '';
-				operator = '';
+			if (firstNumber === "Error!") {
+				firstNumber = "";
+				operator = "";
 			}
-			secondNumber = '';
+			secondNumber = "";
 		}
-	})
-})
+	});
+});
 
-const equalBtn = document.querySelector('.equals')
-equalBtn.addEventListener('click', (e) => {
+const equalBtn = document.querySelector(".equals");
+equalBtn.addEventListener("click", (e) => {
 	if (!firstNumber || !display) {
-		console.log('No mathematical expression')
-		return
+		console.log("No mathematical expression");
+		return;
 	}
 	secondNumber = display;
-	displayScreen.textContent = evaluate(firstNumber, secondNumber, operator)
-	display = '';
-	operator = '';
+	displayScreen.textContent = evaluate(firstNumber, secondNumber, operator);
+	display = "";
+	operator = "";
 	firstNumber = displayScreen.textContent;
-	secondNumber = '';
-})
+	secondNumber = "";
+});
 
 const evaluate = (firstNumber, secondNumber, operator) => {
-	if (operator === '+') {
+	if (operator === "+") {
 		operator = add;
-	} else if (operator === '-') {
+	} else if (operator === "-") {
 		operator = subtract;
-	} else if (operator === '÷') {
+	} else if (operator === "÷") {
 		operator = divide;
-	} else if (operator === '×') {
+	} else if (operator === "×") {
 		operator = multiply;
 	}
 	if (operator === divide && secondNumber == 0) {
-		return 'Error!';
+		return "Error!";
 	}
 	let temp = operate(firstNumber, secondNumber, operator);
 	if (temp.toString().length > 14 && temp % 1 !== 0) {
-		temp = (parseFloat(temp)).toFixed(3);
-		window.alert('Number too huge, rounded to 3 decimal places')
-		return temp
+		temp = parseFloat(temp).toFixed(3);
+		window.alert("Number too huge, rounded to 3 decimal places");
+		return temp;
 	}
-	return temp
-}
+	return temp;
+};
 
-const clearBtn = document.querySelector('.clear')
-clearBtn.addEventListener('click', (e) => {
-	display = '';
-	displayScreen.textContent = '';
-	firstNumber = '';
-	secondNumber = '';
-	operator = '';
-})
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", (e) => {
+	display = "";
+	displayScreen.textContent = "";
+	firstNumber = "";
+	secondNumber = "";
+	operator = "";
+});
